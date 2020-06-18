3cb9c9281bf375a09b7f99e4ced6815db0bd4d16
Josh Ellsworth <joshuaellsworth1@gmail.com>
2020-06-18 15:37:26 -0400

    first commit, deployed app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react App</h1>
+      <h2>Author: Joshua Ellsworth</h2>
+    </div>
+  );
+}
+
+export default App;
