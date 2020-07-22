1540b4d031f5f60c773b723655514848ef223ca6
Josh Ellsworth <joshuaellsworth1@gmail.com>
2020-07-22 14:56:21 -0400

    creating homepage props

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Hero from '../components/Hero'
+
 function HomePage(props) {
 
     return (
-        <p>Homepage Works!</p>
+        <Hero title={props.title} />
     );
 }
 
