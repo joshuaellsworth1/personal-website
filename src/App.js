de52d201227d78db04f5b895757780f67cfe8e24
Josh Ellsworth <joshuaellsworth1@gmail.com>
2020-07-22 13:48:50 -0400

    linked the pages succesfully

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+
 class App extends React.Component {
 
   constructor(props) {
@@ -38,7 +43,7 @@ class App extends React.Component {
           <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Josh Ellsworth</Navbar.Brand>
 
-            <Navbar.Toggle className="border-0" aria-controls='navbar-toggle'/>
+            <Navbar.Toggle className="border-0" aria-controls='navbar-toggle' />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
@@ -48,7 +53,11 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Navbar>
 
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />} />
 
+          <Footer />
 
         </Container>
       </Router>
