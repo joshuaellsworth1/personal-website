70d1125b918fdb5a37a23284df0bfbc56be5a330
Josh Ellsworth <joshuaellsworth1@gmail.com>
2020-08-12 17:32:34 -0400

    changed home route

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Navbar>
 
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/*" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
           <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />} />
           <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} />} />
 
